85da47a0148c456669703dbbb710e47dc80d2933
Douglas Rodrigues <drodrigues@uoldiveo.com>
2016-09-06 16:07:15 -0300

    Lesson 6.

---
+++ 06-docker_registry.sh
@@ -0,0 +1,15 @@
+### create account on hub.docker.com
+
+### Docker login
+docker login
+
+### Tag image to upload on hub.docker.com
+docker tag personalized_nginx docker.io/DOCKER_HUB_USER/personalized_nginx:v0.0.1
+
+### Show your images
+docker images
+
+### Upload image
+docker push docker.io/DOCKER_HUB_USER/personalized_nginx:v0.0.1
+
+# Go to https://hub.docker.com

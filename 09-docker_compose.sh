8403154d1a39f7cbf82f87a3e989022822480ce0
Douglas Rodrigues <drodrigues@uoldiveo.com>
2016-09-06 16:07:51 -0300

    Lesson 9.

---
+++ 09-docker_compose.sh
@@ -0,0 +1,24 @@
+### Installing
+sudo su -
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+docker-compose --version
+
+#########################################################
+### Criate file "docker-compose.yml" with:
+version: "2"
+services:
+  graphite:
+    image: hopsoft/graphite-statsd
+    ports:
+      - "80:80"
+      - "2003-2004:2003-2004"
+
+  grafana:
+    image: grafana/grafana
+    ports:
+      - "3000:3000"
+    links:
+      - graphite
+
+### Create environment
+docker-compose -p ‘treinamento’ up

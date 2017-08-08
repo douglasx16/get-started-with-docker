df7f7ca802d48fa4f16f3079de979fdae8fcfa09
Douglas Rodrigues <drodrigues@uoldiveo.com>
2017-08-08 18:56:51 -0300

    Adjust permision on docker-compose

--- 09-docker_compose.sh
+++ 09-docker_compose.sh
@@ -1,6 +1,7 @@
 ### Installing
 sudo su -
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
 #########################################################
